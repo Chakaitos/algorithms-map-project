2c2c753888c534a76a5812ab81eb1e44ee5629ca
Jose R. Borja <chakaitos@gmail.com>
2014-04-22 16:08:17 -0500

    Add initialize, add_city and add_road methods for the Map class, along with its respective specs

--- map.rb
+++ map.rb
@@ -1,11 +1,22 @@
 class Map
+  attr_accessor :cities
+
+  def initialize
+    @cities = {}
+  end
+
   def add_city(name)
+    if @cities[name].nil?
+      @cities[name] = City.new(name)
+    end
   end
 
   def remove_city(name)
   end
 
   def add_road(origin, destination, distance)
+    @cities[origin].add_road(destination, distance)
+    @cities[destination].add_road(origin, distance)
   end
 
   def remove_road(origin, destination)
@@ -17,6 +28,7 @@ end
 
 class City
   attr_accessor :roads, :name
+
   def initialize(name)
     @name = name
     @roads = {} # hash to map connected Cities with their respective distance
--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -2,28 +2,57 @@ require 'spec_helper.rb'
 
 describe City do
 
-	before do
-	  @atx = City.new("Austin")
-	  @dal = City.new("Dallas")
-	end
+	let(:austin) { City.new("Austin") }
+  let(:dallas) { City.new("Dallas") }
 
   describe "#initialize" do
     it "initializes a city with a name" do
-      expect(@atx.name).to eq("Austin")
+      expect(austin.name).to eq("Austin")
     end
 
     it "initializes with an empty roads hash" do
-      expect(@atx.roads).to eq({})
+      expect(austin.roads).to eq({})
     end
   end
 
   describe "#add_road" do
   	it "adds a road between the origin city and a provided destination" do
-	  	@atx.add_road(@dal.name, 200)
-	  	expect(@atx.roads).to eq({"Dallas" => 200})
+	  	austin.add_road(dallas.name, 200)
+
+	  	expect(austin.roads).to eq({"Dallas" => 200})
   	end
   end
 end
 
 describe Map do
+
+	let(:texas) { Map.new }
+
+	describe "#initialize" do
+		it "initializes with an empty cities hash" do
+			expect(texas.cities).to eq({})
+		end
+	end
+
+	describe "#add_city" do
+		it "adds a city to the cities hash" do
+			texas.add_city("Austin")
+			expect(texas.cities.size).to eq(1)
+
+			texas.add_city("Houston")
+			expect(texas.cities.size).to eq(2)
+		end
+	end
+
+	describe "#add_road" do
+		it "adds a road to both cities that connects them" do
+			austin = texas.add_city("Austin")
+			dallas = texas.add_city("Dallas")
+			binding.pry
+			texas.add_road(austin, dallas, 200)
+
+			expect(texas.cities[austin].roads).to eq({"Dallas" => 200})
+			expect(texas.cities[dallas].roads).to eq({"Austin" => 200})
+		end
+	end
 end
